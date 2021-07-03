601de52170631ecf68ae6c1a85cfe6056cc71324
nilesh.khandwe <nilesh.khandwe@NTINLTP2001317.FPS.NIHILENT.com>
2021-07-03 21:11:15 +0530

    New commit 3 july

---
+++ Introduction/src/ReverseString.java
@@ -0,0 +1,18 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String str = "Nilesh" ;
+		 char[] str1 = str.toCharArray();
+		
+		 for (int i=str1.length-1 ; i>=0 ; i--)
+		 {
+		
+		System.out.println(str1[i]);
+		 }
+	}
+
+}
---
+++ Introduction/src/swap.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x , y , temp ;
+		
+		System.out.println("Enter x and y");
+		Scanner in = new Scanner(System.in);
+		x= in.nextInt();
+		y= in.nextInt();
+		
+		System.out.println("Before swapping"+x +y);
+		
+	/*	temp = x;
+		x=y;
+		y=temp;  */
+		
+		x= x+y;
+		y=x-y;
+		x=x-y;
+		
+		
+		System.out.println("After swapping" +x +y);
+		
+	}
+
+}
